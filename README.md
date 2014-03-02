e7a1a0fd09495e2bfe48b9a50ee5aedc464d29ca
daiying.zhang <daiying.zhang@qunar.com>
2014-03-02 15:41:03 +0800

    version 2.0.0

--- README.md
+++ README.md
@@ -1,74 +1,11 @@
-﻿jquery.jscrollbar
+﻿jquery.jscrollbar 2.0.0
 ========================
 jquery.jscrollbar 是一个基于jQuery的滚动条插件，支持水平滚动条和垂直滚动条，支持鼠标键盘事件
 ------------------------
 
-### 主要功能
-1. 支持水平滚动条
-2. 支持垂直滚动条
-3. 自动判断水平滚动条和垂直滚动条是否显示
-4. 支持外部调用来滚动内容
-5. 支持滚动条部分样式自定义
-6. 支持键盘方向键控制
-7. 支持鼠标滚动(需要mousewheel插件)
-8. 支持滚动条显示位置设置(外部|悬浮)
-9. 支持手动更新界面
+### 友情提示
 
-### 依赖的库
-1. jQuery (http://jquery.com/)
-2. jquery.jqdrag (https://github.com/daiying-zhang/jquery.jqdrag)
-3. jquery.mousewheel (插件已经包含在本项目中，文件：jquery.mousewheel.min.js)
-
-### 使用步骤
-1.在&lt;head&gt;&lt;/head&gt;或者&lt;body&gt;&lt;/body&gt;中引入下列文件:
-
-    <!--必须引入-->
-    <script type="text/javascript" src="your-path/jquery-1.8.1.min.js"></script>
-    <!--如果需要支持鼠标滚动则引入，否则可以不引用-->
-    <script type="text/javascript" src="your-path/require/jquery.mousewheel.min.js"></script>
-    <!--必须引入-->
-    <script type="text/javascript" src="your-path/require/jquery.jqdrag-1.0.min.js"></script>
-    <!--必须引入-->
-    <script type="text/javascript" src="your-path/min/jquery.jscrollbar-1.0.2.min.js"></script>
-
-2.设置内容区域的大小:
-
-    <!--设置区域大小，包括滚动条-->
-    <div style="width:1300px;height:600px;">Some long text or other elements...</div>
-
-3.调用插件：
-
-    $(function(){
-        $('#test1,#test2').jscrollbar({
-            //some options
-        });
-    });
-
-### 示例代码
-    $(function(){
-        $('#test1,#test2').jscrollbar({
-            width:12, //滚动条宽度
-            color:'orange', //滚动条颜色
-            opacity:0.7, //透明度
-            position:'inner', //滚动条位置
-            mouseScrollDirection:'horizontal' //鼠标滚动时滚动的方向
-        });
-
-        var jsb2 = $('#test2').jscrollbar('getObject');
-
-        setTimeout(function(){
-            $('#test2 img').css({width:'4000px'});
-                //滚动实例的链式调用，无法使用jQuery操作DOM的方法 [不推荐]
-                jsb2.updateUI()
-                     .scrollTo('x',100)
-                     .scrollBy('x',50);
-
-                //jQuery的链式调用，可以使用jQuery操作DOM的方法  [推荐]
-                $('#test1').jscrollbar('scrollBy','x',10)
-                           .jscrollbar('scrollTo','x',300)
-                           .animate({'opacity':0.8},1000);
-        },2000)
-    });
+2.0.0 正在开发，请期待...
 
 ### E-Mail
 
---
+++ src/jquery.jscrollbar.js
@@ -0,0 +1,220 @@
+﻿/**
+ *
+ * Version : 2.0.0
+ */
+
+;(function($){
+
+    var DATA_NAME = 'jsb_data',
+        BAR_WRAPPER_NODE = $('<div class="jscrollbar" style="overflow: hidden"></div>'),
+        THUMB_NODE = '<div class="thumb"></div>',
+        //bars = '', delta = 0,
+        //thumbSize = null,maxPos = null,
+        MAPPING = {
+            x : {s  : 'width', p  : 'left', sp : 'scrollLeft', ss : 'scrollWidth'},
+            y : {s  : 'height', p  : 'top', sp : 'scrollTop', ss : 'scrollHeight'}
+        };
+
+    function setProp(width, height, barWidth){
+        this.bars = testXYShow.call(this, width, height, barWidth);
+        var delta = this.delta = (this.bars.length - 1) * this.opt.width;
+        var thumbSize = this.thumbSize = {x:getThumbSize.call(this, 'x'),y:getThumbSize.call(this, 'y')};
+
+        this.maxPos = {x: this.width - thumbSize.x - delta, y: this.height - thumbSize.y - delta};
+        this.maxSPos = {x: this.scrollWidth - this.width + delta, y: this.scrollHeight - this.height + delta}
+        console.log(this.scrollHeight, this.height);
+        //console.log(this.plugID, delta, this.bars);
+    }
+
+    function addScrollbar($node, width, height, barWidth){
+
+        setProp.call(this, width, height, barWidth);
+
+        var type = this.bars.split(''),
+            mapping = {'x':'height','y':'width'},
+            className,
+            size = 0, i= 0,
+            len = type.length;
+        //console.log(type);
+        for(; i < len; i++){
+            //debugger;
+            className = type[i];
+            size = this.thumbSize[className];
+            //this[mapping[className]] -= this.opt.width;
+            //加入滚动条背景容器
+            $('<div class="' + className + '"></div>')
+                .css(mapping[className] ,barWidth)
+                //插入拖动条
+                .append($(THUMB_NODE)[mapping['xy'.replace(className,'')]](size).data('type', className))
+                .insertAfter($node.css(mapping[className], '-='+barWidth))
+        }
+    }
+
+    function getThumbSize(type){
+        // this.width : this.scrollWidth = w : this.width
+        var prop = {'x' : 'Width', 'y' : 'Height'}[type],
+            size = this.$node[prop.toLowerCase()](),
+            //delta = bars.length === 2 ? this.opt.width : 0;
+            delta = this.delta;
+        return Math.max(10, Math.pow(size - delta, 2) / this.node['scroll' + prop]);
+    }
+
+    function init(width, height, barWidth){
+        this.$node.css('overflow','hidden').wrap(BAR_WRAPPER_NODE.attr('id', this.plugID).css({width:width, height:height}));
+        addScrollbar.call(this, this.$node, width, height, barWidth);
+        initEvent.call(this);
+    }
+
+    function testXYShow(width, height, barWidth){
+        //if(!this.showXBar && !this.showYBar) return;
+
+        var node = this.node,
+            $node = this.$node,
+            $cloneNode = $node.clone(),
+            xflag = node.scrollWidth > width,
+            yflag = node.scrollHeight > height,
+            tmp = 0, type = '';
+        //todo 优化：如果设置了不显示某个滚动条，就不用检测
+        if(xflag && yflag){
+            //初始就有两个滚动条
+            type = 'xy'
+        }else if(xflag){
+            //初始时只有水平滚动条
+            tmp = $cloneNode.css({'height' : height - barWidth, 'zIndex' : -1}).appendTo($('body'))[0].scrollHeight;
+            type = 'x' + (tmp > height - barWidth ? 'y' : '')
+        }else{
+            //初始时只有垂直滚动条
+            tmp = $cloneNode.css({'width' : width - barWidth, 'zIndex' : -1}).appendTo($('body'))[0].scrollWidth;
+            type = (tmp > width -barWidth ? 'x' : '') + 'y'
+        }
+
+        this.scrollWidth = $cloneNode[0].scrollWidth;
+        this.scrollHeight = $cloneNode[0].scrollHeight;
+        $cloneNode.remove();
+        return  type;
+        //console.log(width, widthContent);
+        //console.log(height, realHeight);
+    }
+
+    function initEvent(){
+        var self = this;
+        $('#'+this.plugID).on('mousedown','.thumb',function(eve){
+            start.call(self, eve, eve.target);
+            return false
+        })
+
+        //todo 优化event bind
+        $(document).unbind('mouseup.jsb').bind('mouseup.jsb', function(){
+            console.log('up');
+            $(this).unbind('mousemove.jsb');
+        })
+    }
+
+    function start(eve, ele){
+        var start = {X: eve.pageX, Y: eve.pageY},
+            self = this,
+            pos = {
+                X : parseInt($(ele).css('left')) || 0,
+                Y : parseInt($(ele).css('top')) || 0
+            };
+        $(document).bind('mousemove.jsb', function(eve){
+            var type = $.data(ele, 'type').toUpperCase(),
+                mouseDelta = eve['page'+type] - start[type],
+                cssProp = type === 'X' ? 'left' : 'top',
+                cssValue = Math.min(Math.max(0, pos[type] + mouseDelta),self.maxPos[type.toLowerCase()]);
+            $(ele).css(cssProp, cssValue);
+            self.scroll(type.toLowerCase())
+        })
+    }
+
+
+    function JScrollBar($node, opt){
+        /**
+         * 添加滚动条的元素
+         * @type {jQuery}
+         */
+        this.node = $node[0];
+        this.$node = $node;
+        this.width = $node.width();
+        this.height = $node.height();
+        this.opt = opt;
+        this.plugID = 'jsb_' + Math.floor(Math.random() * 9000 + 1000);
+
+        //self = this;
+        init.call(this, this.width, this.height, opt.width);
+    }
+
+
+    JScrollBar.fn = JScrollBar.prototype;
+
+    JScrollBar.fn.update = function(type){
+
+    }
+
+    JScrollBar.fn.getThumbLocation = function(direction){
+        var i = 0,len = direction.length,$ele = null,pos = 0;
+        for(;i<len;i++){
+            $ele = $('#'+this.plugID).find('.' + direction.substr(i,1) + ' .thumb');
+            pos = $ele.css(direction === 'x' ? 'left' : 'top')
+        }
+        return parseFloat(pos)
+    }
+
+    JScrollBar.fn.scroll = function(direction){
+        var pos = this.getThumbLocation(direction);
+        var mapObj = MAPPING[direction];
+        console.log(pos, this.maxPos[direction], this.maxSPos[direction]);
+        this.node[mapObj.sp] = pos / this.maxPos[direction] * this.maxSPos[direction];
+    }
+
+    JScrollBar.fn.scrollBy = function(){
+
+    }
+
+    JScrollBar.fn.scrollTo = function(){
+
+    }
+
+    /** @name $.fn.jscrollbar
+    * @param {Object} [options] 配置参数
+    * @param {String} [options._color='black'] 滚动条的颜色
+    * @param {Number} [options.width=12] 滚动条的宽度
+    * @param {Number} [options.opacity=0.6] 滚动条的透明度(0~1)
+    * @param {Number} [options.borderRadius=6] 滚动条圆角大小
+    * @param {String} [options.position='outer'] 滚动条显示位置
+    * @param {String} [options.showXBar=true] 是否显示水平滚动条
+    * @param {String} [options.showYBar=true] 是否显示垂直滚动条
+    * @param {String} [options.keyControl=true] 是否接受键盘按键(上下左右键)控制
+    * @param {String} [options.keyMoveAmount=30] 键盘控制每次移动的距离(单位:px)
+    * @param {String} [options.mouseScrollDirection='vertical'] 鼠标滚动时控制的滚动方向(上下滚动或者左右滚动)
+    * @param {String} [options.mouseMoveAmount=30] 鼠标滚动时每次移动的距离(单位:px)
+    * @returns {*}
+    * @example
+    * $('#ele').jscrollbar({
+    *      _color:'#09F',
+        *      opacity:0.7
+    * })*/
+
+    $.fn.jscrollbar = function(opt){
+        opt = $.extend({},{
+            //default params
+            'width' : 12,
+            'position' : 'outer',
+            'showXBar' : true,
+            'showYBar' : true,
+            'keyControl' : true,
+            'keyMoveAmount' : 30,
+            'mouseControl' : true,
+            'mouseScrollDirection' : 'vertical',
+            'mouseMoveAmount' : 30
+        },opt);
+
+        return this.each(function(){
+            var objData = $.data(this, DATA_NAME);
+            if(!objData){
+                $.data(this, DATA_NAME, new JScrollBar($(this), opt))
+            }
+        })
+    }
+    $.fn.jscrollbar.version = '2.0.0';
+})(jQuery);
